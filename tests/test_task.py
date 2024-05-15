6783e510ca711b561c92954989c58a0da26242f6
Natalia Orsi <naty.m.orsi@gmail.com>
2024-05-15 14:15:17 +0200

    Added test to tasks with cache

--- tests/test_task.py
+++ tests/test_task.py
@@ -39,4 +39,43 @@ def test_tasks_no_cache(tmp_path, capsys):
 
 def test_tasks_with_cache(tmp_path, capsys):
     # TODO: create test that start with existing populated cache
-    pass
+    cache_path = tmp_path / "tasks.txt"
+
+    with open(cache_path, "w", encoding="utf8") as cache_file:
+        contents = textwrap.dedent("""\
+            foo
+            bar
+            """)
+        cache_file.write(contents)
+
+    assert cache_path.is_file()
+
+    task = Task(cache_path=cache_path)
+    assert task.tasks == ["foo", "bar"]
+
+    task.add_task("baz")
+    assert task.tasks == ["foo", "bar", "baz"]
+
+    task.print_tasks()
+    output = textwrap.dedent("""\
+        foo
+        bar
+        baz
+        """)
+    assert capsys.readouterr().out == output
+
+    task.save_tasks()
+    assert cache_path.is_file()
+    assert cache_path.read_text(encoding="utf8") == output
+
+    task.delete_tasks(3)
+    assert task.tasks == ["foo", "bar"]
+
+    task.delete_tasks(2)
+    assert task.tasks == ["foo"]
+
+    task.delete_tasks(1)
+    assert task.tasks == []
+
+    task.load_tasks()
+    assert task.tasks == ["foo", "bar", "baz"]
